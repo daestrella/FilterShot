dc8a2a8d2a525226906c7c5003fc35f0b2d2245c
daestrella <da.estrella@yahoo.com>
2024-10-01 22:12:49 +0800

    Added project with main activity

---
+++ app/src/main/java/com/pup/filtershot/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.pup.filtershot
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.pup.filtershot.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+        binding.bottomNavigationView.setOnItemSelectedListener{
+            when(it.itemId){
+
+                R.id.home -> replaceFragment(Home())
+                R.id.live -> replaceFragment(Live())
+                R.id.record -> replaceFragment(Record())
+
+                else ->{}
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment : Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+    }
+}
