71440a6079be90ed8c81eb2ec7260281b6c3ec50
0ventoaster <zhexxalharvey@gmail.com>
2024-10-01 23:05:08 +0800

    Fixed Fragments not changing

--- app/src/main/java/com/pup/filtershot/MainActivity.kt
+++ app/src/main/java/com/pup/filtershot/MainActivity.kt
@@ -1,40 +1,36 @@
 package com.pup.filtershot
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import com.pup.filtershot.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         replaceFragment(Home())
 
-        binding.bottomNavigationView.setOnItemSelectedListener{
-            when(it.itemId){
-
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
                 R.id.home -> replaceFragment(Home())
                 R.id.live -> replaceFragment(Live())
                 R.id.record -> replaceFragment(Record())
-
-                else ->{}
+                else -> {}
             }
             true
         }
     }
 
-    private fun replaceFragment(fragment : Fragment){
+    private fun replaceFragment(fragment: Fragment) {
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
         fragmentTransaction.commit()
     }
 }
